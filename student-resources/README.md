31287278c020cd1f150d19f7b95b21d013659b9e
wilm0x42 <wilm0x42@startmail.com>
2018-01-28 23:15:49 -0500

    Scrutinize the assignments

---
+++ student-resources/README.md
@@ -0,0 +1,16 @@
+# Student Resources
+
+## What the heck is everything in here
+
+  file           |  explaination
+-----------------|--------------
+pages.txt        | Contains a list of URLS to be used for the assignments
+makebasefiles.sh | Downloads and processes URLS listed in `pages.txt`, outputting them to `basefiles/`
+redohtml/        | Code designed to pull content out of specific web pages, putting them into our own frame.
+home/            | Files that will be copied to student's `$HOME`
+
+## How this works?
+
+* Run makebasefiles.sh
+* Edit the pages in `basefiles/` to our liking.
+* Place them in `home/`
--- student-resources/makebasefiles.sh
+++ student-resources/makebasefiles.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-if [ -d tmp ]; then
-	echo -n
-else
-	mkdir tmp
-fi
-
 if [ -d basefiles ]; then
 	echo -n
 else
 	mkdir basefiles
 fi
 
-cat pages.txt > tmp/lines
 curLine=0
 
-#cd tmp
-while [ $(wc -l tmp/lines | cut -d" " -f1) -gt $curLine ]; do
+while [ $(wc -l pages.txt | cut -d" " -f1) -gt $curLine ]; do
 	curLine=$(($curLine+1))
-	l=$(head tmp/lines --lines=$curLine | tail --lines=1)
+	l=$(head pages.txt --lines=$curLine | tail --lines=1)
 	
 	if [ -z "$l" ]; then
 		continue;
 	fi
 	
-	wget -O redohtml/input.html $l
+	echo Downloading $l
+	wget -O redohtml/input.html -q $l
 	cd redohtml
 	./generatepage.sh
 	cd ..
